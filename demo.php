dd1b2d883fc1856121836e04590002fb7427205d
QcloudApi <QcloudApi@tencent.com>
2014-11-13 09:43:57 +0800

    代码缩进统一

--- demo.php
+++ demo.php
@@ -5,7 +5,7 @@ require_once './src/QcloudApi/QcloudApi.php';
 
 $config = array('SecretId'       => '你的secretId',
                 'SecretKey'      => '你的secretKey',
-				'RequestMethod'  => 'GET',
+                'RequestMethod'  => 'GET',
                 'DefaultRegion'  => 'gz');
 
 $cvm = QcloudApi::load(QcloudApi::MODULE_CVM, $config);
@@ -16,14 +16,13 @@ $a = $cvm->DescribeInstances($package);
 // $a = $cvm->generateUrl('DescribeInstances', $package);
 
 if ($a === false) {
-	$error = $cvm->getError();
-	echo "Error code:" . $error->getCode() . ".\n";
+    $error = $cvm->getError();
+    echo "Error code:" . $error->getCode() . ".\n";
     echo "message:" . $error->getMessage() . ".\n";
     echo "ext:" . var_export($error->getExt(), true) . ".\n";
 } else {
-	var_dump($a);
+    var_dump($a);
 }
-
 
 echo "\nRequest :" . $cvm->getLastRequest();
 echo "\nResponse :" . $cvm->getLastResponse();
