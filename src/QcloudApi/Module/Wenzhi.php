0a72832f8e66d07e30454079cce2111cb5206cdc
QcloudApi <QcloudApi@users.noreply.github.com>
2015-07-14 17:48:06 +0800

    Update Wenzhi.php

--- src/QcloudApi/Module/Wenzhi.php
+++ src/QcloudApi/Module/Wenzhi.php
@@ -1,8 +1,8 @@
 <?php
 require_once QCLOUDAPI_ROOT_PATH . '/Module/Base.php';
 /**
- * QcloudApi_Module_Vod
- * 视频云模块类
+ * QcloudApi_Module_WENZHI
+ * 文智模块类
  */
 class QcloudApi_Module_Wenzhi extends QcloudApi_Module_Base
 {
